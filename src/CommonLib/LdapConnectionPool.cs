990136abe7087d6f79c3f25b4dbf3d97fc84c68b
Alex Nemeth <80649445+definitelynotagoblin@users.noreply.github.com>
2025-01-09 08:49:48 -0800

    [BP-1232] Connectionpool blacklist (#181)
    * BP-1232: break connection attempts to domain '.'
    
    * Add domain blacklist to LdapConnectionPool to try to alleviate pain from repeated query attempts to a domain we can't reach
    
    * Check blacklist against global catalog connections too
    
    * Renaming blacklisted domains => excluded domains
    
    * Adding comment to explain use for _excludedDomains

--- src/CommonLib/LdapConnectionPool.cs
+++ src/CommonLib/LdapConnectionPool.cs
@@ -32,6 +32,9 @@ namespace SharpHoundCommonLib {
         private const int MaxRetries = 3;
         private static readonly ConcurrentDictionary<string, NetAPIStructs.DomainControllerInfo?> DCInfoCache = new();
 
+        // Tracks domains we know we've determined we shouldn't try to connect to
+        private static readonly ConcurrentHashSet _excludedDomains = new();
+
         public LdapConnectionPool(string identifier, string poolIdentifier, LdapConfig config, PortScanner scanner = null, NativeMethods nativeMethods = null, ILogger log = null) {
             _connections = new ConcurrentBag<LdapConnectionWrapper>();
             _globalCatalogConnection = new ConcurrentBag<LdapConnectionWrapper>();
@@ -595,6 +598,10 @@ namespace SharpHoundCommonLib {
         }
 
         public async Task<(bool Success, LdapConnectionWrapper ConnectionWrapper, string Message)> GetConnectionAsync() {
+            if (_excludedDomains.Contains(_identifier)) {
+                return (false, null, $"Identifier {_identifier} excluded for connection attempt");
+            }
+
             if (!_connections.TryTake(out var connectionWrapper)) {
                 var (success, connection, message) = await CreateNewConnection();
                 if (!success) {
@@ -613,6 +620,10 @@ namespace SharpHoundCommonLib {
         }
 
         public async Task<(bool Success, LdapConnectionWrapper ConnectionWrapper, string Message)> GetGlobalCatalogConnectionAsync() {
+            if (_excludedDomains.Contains(_identifier)) {
+                return (false, null, $"Identifier {_identifier} excluded for connection attempt");
+            }
+
             if (!_globalCatalogConnection.TryTake(out var connectionWrapper)) {
                 var (success, connection, message) = await CreateNewConnection(true);
                 if (!success) {
@@ -691,6 +702,7 @@ namespace SharpHoundCommonLib {
                     _log.LogDebug(
                         "Could not get domain object from GetDomain, unable to create ldap connection for domain {Domain}",
                         _identifier);
+                    _excludedDomains.Add(_identifier);
                     return (false, null, "Unable to get domain object for further strategies");
                 }
                 tempDomainName = domainObject.Name.ToUpper().Trim();
@@ -725,6 +737,7 @@ namespace SharpHoundCommonLib {
                 }
             } catch (Exception e) {
                 _log.LogInformation(e, "We will not be able to connect to domain {Domain} by any strategy, leaving it.", _identifier);
+                _excludedDomains.Add(_identifier);
             }
 
             return (false, null, "All attempted connections failed");
