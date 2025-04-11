26001a33e0b526fe2c05064ef63c83773c426eaf
Katie Strader <kstrader@specterops.io>
2025-04-11 12:19:07 -0700

    NTLM Unit Tests (#194)
    * chore: add unit tests for NTLM
    
    * chore: add unit tests for NTLM
    
    * chore: fix SmbProcessorTest.cs
    
    * chore: using internal instead of public

--- src/CommonLib/Processors/CAEnrollmentProcessor.cs
+++ src/CommonLib/Processors/CAEnrollmentProcessor.cs
@@ -107,7 +107,7 @@ namespace SharpHoundCommonLib.Processors {
             return endpoints;
         }
 
-        private (Uri httpUrl, Uri httpsUrl) BuildEnrollmentUrls(CAEnrollmentEndpointType type) {
+        internal (Uri httpUrl, Uri httpsUrl) BuildEnrollmentUrls(CAEnrollmentEndpointType type) {
             switch (type) {
                 case CAEnrollmentEndpointType.WebEnrollmentApplication:
                     return (new Uri($"http://{_caDnsHostname}/certsrv/"),
---
+++ test/unit/WebClientServiceProcessorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Moq;
+using SharpHoundCommonLib;
+using SharpHoundCommonLib.OutputTypes;
+using SharpHoundCommonLib.Processors;
+using SharpHoundRPC;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CommonLibTest {
+    [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
+    public class WebClientServiceProcessorTest : IDisposable {
+
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public WebClientServiceProcessorTest(ITestOutputHelper testOutputHelper) {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        public void Dispose() {
+        }
+    
+        [Fact]
+        public async Task WebClientServiceProcessorTest_TestPathExists()
+        {
+            var processor = new WebClientServiceProcessor();
+            
+            var result = await processor.IsWebClientRunning("primary.testlab.local");
+            
+            Assert.True(result.Collected);
+            Assert.False(result.Result);
+        }
+        
+    }
+}
