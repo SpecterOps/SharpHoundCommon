61e8660248331988752bf2987a883ffb5260987e
Jonas Bülow Knudsen <jknudsen@specterops.io>
2025-04-15 17:17:16 +0200

    add netbios for domains

--- src/CommonLib/LdapQueries/CommonProperties.cs
+++ src/CommonLib/LdapQueries/CommonProperties.cs
@@ -60,7 +60,7 @@
             LDAPProperties.SupportedEncryptionTypes, LDAPProperties.DSHeuristics,
             LDAPProperties.MinPwdLength, LDAPProperties.PwdProperties, LDAPProperties.MinPwdAge, 
             LDAPProperties.MaxPwdAge, LDAPProperties.PwdHistoryLength, LDAPProperties.LockoutDuration, 
-            LDAPProperties.LockoutThreshold, LDAPProperties.LockOutObservationWindow
+            LDAPProperties.LockoutThreshold, LDAPProperties.LockOutObservationWindow, LDAPProperties.PrincipalName
         };
 
         public static readonly string[] ContainerProps =
