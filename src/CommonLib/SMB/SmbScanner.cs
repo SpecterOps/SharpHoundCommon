bd33ad1dd4f1c9b9b3a3af3f8498cfaed48f4822
anemeth <anemeth@specterops.io>
2025-05-28 13:34:22 -0700

    Small logging info polish for SMB2 StructureSize message

--- src/CommonLib/SMB/SmbScanner.cs
+++ src/CommonLib/SMB/SmbScanner.cs
@@ -262,7 +262,7 @@ namespace SharpHoundCommonLib.SMB
 
             if (negotiateStructureSize != SMB2Constants.ExpectedNegotiateStructureSizeB)
             {
-                _log.LogDebug($"Expected fixed-value SMB2 response structure size, got {negotiateStructureSize}. Packet: {Convert.ToBase64String(responsePacket)}");
+                _log.LogDebug($"Expected fixed-value SMB2 response structure size {SMB2Constants.ExpectedNegotiateStructureSizeB}, got {negotiateStructureSize}. Packet: {Convert.ToBase64String(responsePacket)}");
                 return (true, false);
             }
 
