d0540d419691b83e886a3ba1fce49324029728cc
anemeth <anemeth@specterops.io>
2025-06-09 11:10:19 -0700

    Let's also check that exactly 2 filters are returned by our MergeFilter test

--- test/unit/LDAPFilterTest.cs
+++ test/unit/LDAPFilterTest.cs
@@ -102,6 +102,8 @@ namespace CommonLibTest
             // Check that each of userFilter and computerFilter are accounted for
             Assert.Single(filters.Where(f => f.Contains(userFilter)));
             Assert.Single(filters.Where(f => f.Contains(computerFilter)));
+
+            Assert.Equal(2, filters.Count());
         }
 
         #endregion
