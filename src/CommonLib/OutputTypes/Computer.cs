2c2350e93ee25b6e166224eb97ddd0b44099118d
anemeth <anemeth@specterops.io>
2025-06-27 10:01:48 -0700

    feat: Add isReadOnlyDC property to computer objects

--- src/CommonLib/OutputTypes/Computer.cs
+++ src/CommonLib/OutputTypes/Computer.cs
@@ -19,6 +19,7 @@ namespace SharpHoundCommonLib.OutputTypes {
         public DCRegistryData DCRegistryData { get; set; } = new();
         public ComputerStatus Status { get; set; }
         public bool IsDC { get; set; }
+        public bool IsReadOnlyDC { get; set; }
         public bool UnconstrainedDelegation { get; set; }
         public string DomainSID { get; set; }
 
