7800926b6fc8e243199482967dad0fb1a766dc5d
anemeth <anemeth@specterops.io>
2025-07-11 11:59:18 -0700

    chore: Add AdaptiveTimeout unit tests

---
+++ test/unit/AdaptiveTimeoutTest.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Threading.Tasks;
+using SharpHoundCommonLib;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace CommonLibTest;
+
+public class AdaptiveTimeoutTest {
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public AdaptiveTimeoutTest(ITestOutputHelper testOutputHelper) {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public async Task AdaptiveTimeout_GetAdaptiveTimeout_NotEnoughSamplesAsync() {
+        var maxTimeout = TimeSpan.FromSeconds(1);
+        var adaptiveTimeout = new AdaptiveTimeout(maxTimeout, new TestLogger(_testOutputHelper, Microsoft.Extensions.Logging.LogLevel.Trace), 10, 1000, 3);
+
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(50));
+
+        var adaptiveTimeoutResult = TestPrivateMethod.InstanceMethod<TimeSpan>(adaptiveTimeout, "GetAdaptiveTimeout", []);
+        Assert.Equal(maxTimeout, adaptiveTimeoutResult);
+    }
+
+    [Fact]
+    public async Task AdaptiveTimeout_GetAdaptiveTimeout_AdaptiveDisabled() {
+        var maxTimeout = TimeSpan.FromSeconds(1);
+        var adaptiveTimeout = new AdaptiveTimeout(maxTimeout, new TestLogger(_testOutputHelper, Microsoft.Extensions.Logging.LogLevel.Trace), 10, 1000, 3, false);
+
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(50));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(50));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(50));
+
+        var adaptiveTimeoutResult = TestPrivateMethod.InstanceMethod<TimeSpan>(adaptiveTimeout, "GetAdaptiveTimeout", []);
+        Assert.Equal(maxTimeout, adaptiveTimeoutResult);
+    }
+
+    [Fact]
+    public async Task AdaptiveTimeout_GetAdaptiveTimeout() {
+        var maxTimeout = TimeSpan.FromSeconds(1);
+        var adaptiveTimeout = new AdaptiveTimeout(maxTimeout, new TestLogger(_testOutputHelper, Microsoft.Extensions.Logging.LogLevel.Trace), 10, 1000, 3);
+
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(40));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(50));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(60));
+
+        var adaptiveTimeoutResult = TestPrivateMethod.InstanceMethod<TimeSpan>(adaptiveTimeout, "GetAdaptiveTimeout", []);
+        Assert.True(adaptiveTimeoutResult < maxTimeout);
+    }
+
+    [Fact]
+    public async Task AdaptiveTimeout_GetAdaptiveTimeout_TimeSpikeSafetyValve() {
+        var maxTimeout = TimeSpan.FromSeconds(1);
+        var numSamples = 30;
+        var adaptiveTimeout = new AdaptiveTimeout(maxTimeout, new TestLogger(_testOutputHelper, Microsoft.Extensions.Logging.LogLevel.Trace), numSamples, 1000, 3);
+
+        for (int i = 0; i < numSamples; i++)
+            await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(10));
+
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(100));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(100));
+        await adaptiveTimeout.ExecuteWithTimeout(async (_) => await Task.Delay(100));
+
+        var adaptiveTimeoutResult = TestPrivateMethod.InstanceMethod<TimeSpan>(adaptiveTimeout, "GetAdaptiveTimeout", []);
+        Assert.Equal(maxTimeout, adaptiveTimeoutResult);
+    }
+}
