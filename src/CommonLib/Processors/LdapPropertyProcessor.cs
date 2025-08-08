41de7912f900abd7f2e8eb49d68e01c198a1b8e7
Michael Cuomo <mcuomo@specterops.io>
2025-08-08 16:45:30 -0400

    WIP: Evaluated and Collect GPO Status on ReadGPOLocalGroups

--- src/CommonLib/Processors/LdapPropertyProcessor.cs
+++ src/CommonLib/Processors/LdapPropertyProcessor.cs
@@ -165,9 +165,25 @@ namespace SharpHoundCommonLib.Processors {
             var props = GetCommonProps(entry);
             entry.TryGetProperty(LDAPProperties.GPCFileSYSPath, out var path);
             props.Add("gpcpath", path.ToUpper());
+            entry.TryGetProperty(LDAPProperties.Flags, out var flags);
+            props.Add("gpostatus", TranslateGPOFlag(flags));
             return props;
         }
 
+        private static string TranslateGPOFlag(string flag) {
+            switch (flag) {
+                case "0":
+                    return "Enabled";
+                case "1":
+                    return "User configuration disabled";
+                case "2":
+                    return "Computer configuration disabled";
+                case "3":
+                default:
+                    return "Disabled";
+            }
+        }
+        
         /// <summary>
         ///     Reads specific LDAP properties related to OUs
         /// </summary>
