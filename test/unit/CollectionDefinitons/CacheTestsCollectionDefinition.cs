0609651eda62f35608c83a537a1bf7221800f535
Emmanuel <eroblescruz@specterops.io>
2025-12-16 16:43:44 -0500

    made some test the were using cache run in sequence so they dont step on eachother

--- test/unit/CollectionDefinitons/CacheTestsCollectionDefinition.cs
+++
@@ -1,9 +0,0 @@
-﻿using Xunit;
-
-namespace CommonLibTest.CollectionDefinitons;
-
-/// <summary>
-/// Test that use cache cannot run in parallel, they can have flaky behavior
-/// </summary>
-[CollectionDefinition(nameof(CacheTestCollectionDefinition), DisableParallelization = true)]
-public class CacheTestCollectionDefinition{}
