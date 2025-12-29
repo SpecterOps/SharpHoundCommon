ee826ce4aee5e154b1c90dfa3e9f41f91ffc6a02
Michael Cuomo <mcuomo@specterops.io>
2025-12-29 11:27:48 -0500

    chore: alter histogram value type

--- src/CommonLib/Models/Metric.cs
+++ src/CommonLib/Models/Metric.cs
@@ -4,13 +4,24 @@ namespace SharpHoundCommonLib.Models;
 
 public abstract record Metric {
     private Metric() { }
-    
-    public sealed record DoubleMetric(string Name, MetricType Type, double Value, IDictionary<string, string> Labels) : Metric;
-    public sealed record VectorMetric(string Name, MetricType Type, IEnumerable<double> Value, IDictionary<string, string> Labels) : Metric;
+
+    public sealed record DoubleMetric(
+        string Name,
+        MetricType Type,
+        double Value,
+        IDictionary<string, string> Labels) : Metric;
+
+    public sealed record DictionaryMetric(
+        string Name,
+        MetricType Type,
+        IDictionary<string, double> Value,
+        IDictionary<string, string> Labels) : Metric;
 }
 
 public enum MetricType {
     Counter,
     Gauge,
-    Histogram
+    ClassicHistogram,
+    // Currently Native Histograms are not supported for scraping.
+    // Histogram,
 }
--- src/CommonLib/Processors/MetricProcessor.cs
+++ src/CommonLib/Processors/MetricProcessor.cs
@@ -23,8 +23,8 @@ public class MetricProcessor(IMetricWriter writer) : IMetricProcessor {
             MetricType.Counter => new Metric.DoubleMetric(name, metricType, -1, labels),
             MetricType.Gauge when value is double v => new Metric.DoubleMetric(name, metricType, v, labels),
             MetricType.Gauge => new Metric.DoubleMetric(name, metricType, -1, labels),
-            MetricType.Histogram when value is IEnumerable<double> v => new Metric.VectorMetric(name, metricType, v, labels),
-            MetricType.Histogram => new Metric.VectorMetric(name, metricType, [], labels),
+            MetricType.ClassicHistogram when value is IDictionary<string, double> v => new Metric.DictionaryMetric(name, metricType, v, labels),
+            MetricType.ClassicHistogram => new Metric.DictionaryMetric(name, metricType, [], labels),
             _ => new Metric.DoubleMetric(name, metricType, -1, labels)
         };
 
@@ -35,11 +35,9 @@ public class MetricProcessor(IMetricWriter writer) : IMetricProcessor {
             Metric.DoubleMetric m when metricType is MetricType.Gauge && value is double v =>
                 m with { Labels = CombineLabels(m.Labels, labels), Value = v },
             Metric.DoubleMetric m when metricType is MetricType.Counter => m,
-            Metric.VectorMetric m when metricType is MetricType.Histogram && value is double v =>
-                m with { Labels = CombineLabels(m.Labels, labels), Value = m.Value.Concat([v]) },
-            Metric.VectorMetric m when metricType is MetricType.Histogram && value is IEnumerable<double> v =>
-                m with { Labels = CombineLabels(m.Labels, labels), Value = m.Value.Concat(v) },
-            Metric.VectorMetric m => m,
+            Metric.DictionaryMetric m when metricType is MetricType.ClassicHistogram && value is IDictionary<string, double> v =>
+            m with { Labels = CombineLabels(m.Labels, labels), Value = CombineObservations(m.Value, v) },
+            Metric.DictionaryMetric m => m,
             _ => new Metric.DoubleMetric(name, metricType, -1, labels)
         };
 
@@ -48,7 +46,20 @@ public class MetricProcessor(IMetricWriter writer) : IMetricProcessor {
         if (labels1 != null && labels2 != null) {
              return labels1.Concat(labels2.Where( x=> !labels1.ContainsKey(x.Key))).ToDictionary(x=>x.Key, x=>x.Value);
         }
-        return labels1 ?? labels2;
+        return labels1 ?? labels2 ?? [];
+    }
+
+    private static IDictionary<string, double> CombineObservations(IDictionary<string, double> observations1 = null,
+        IDictionary<string, double> observations2 = null) {
+        if (observations1 == null || observations2 == null) return observations1 ?? observations2 ?? [];
+        foreach (var pair in observations2) {
+            if (!observations1.ContainsKey(pair.Key))
+                observations1[pair.Key] = pair.Value;
+            else
+                observations1[pair.Key] += pair.Value;
+        }
+
+        return observations1;
     }
 
     public async Task FlushAsync() {
