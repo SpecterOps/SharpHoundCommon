0ab6df679be7567bc1d2179eaca8e2e3ef675907
Lucas Falslev <lfalslev@specterops.io>
2026-03-13 11:52:02 -0600

    make exception nullable

--- test/unit/Facades/MockExtensions.cs
+++ test/unit/Facades/MockExtensions.cs
@@ -44,7 +44,7 @@ public static class MockExtensions
                 It.IsAny<EventId>(),
                 It.IsAny<It.IsAnyType>(),
                 It.IsAny<Exception>(),
-                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+                It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
             Times.Never);
     }
 }
