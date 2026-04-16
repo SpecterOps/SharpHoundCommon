fdd47436bb243c642738cb2e761531cc6b241c42
Lucas Falslev <lfalslev@specterops.io>
2026-04-15 23:44:11 -0600

    remove test breaking change

--- src/CommonLib/Processors/ACLProcessor.cs
+++ src/CommonLib/Processors/ACLProcessor.cs
@@ -42,7 +42,7 @@ namespace SharpHoundCommonLib.Processors {
             };
         }
 
-        public ACLProcessor(ILdapUtils utils, string TEST_BREAKING_CHANGE, ILogger log = null)
+        public ACLProcessor(ILdapUtils utils, ILogger log = null)
         {
             _utils = utils;
             _log = log ?? Logging.LogProvider.CreateLogger("ACLProc");
