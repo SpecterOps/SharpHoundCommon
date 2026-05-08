9b276ee82bc849c4325ea486ca12eed923c0093a
Lucas Falslev <lfalslev@specterops.io>
2026-05-08 11:32:36 -0600

    materialize filter list to avoid multiple enumeration

--- test/unit/LDAPFilterTest.cs
+++ test/unit/LDAPFilterTest.cs
@@ -86,7 +86,7 @@ namespace CommonLibTest
             test.AddFilter(mandatoryFilter1, true);
             test.AddFilter(mandatoryFilter2, true);
 
-            IEnumerable<string> filters = test.GetFilterList();
+            IReadOnlyList<string> filters = test.GetFilterList().ToList();
 
             string computerFilter = "(samaccounttype=805306369)";
             string userFilter = "(|(samaccounttype=805306368)(samaccounttype=805306370))";
@@ -103,7 +103,7 @@ namespace CommonLibTest
             Assert.Single(filters, f => f.Contains(userFilter));
             Assert.Single(filters, f => f.Contains(computerFilter));
 
-            Assert.Equal(2, filters.Count());
+            Assert.Equal(2, filters.Count);
         }
 
         #endregion
